233fc4b94ee304773762cfad43dd44d8a568b2a9
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2024-02-05 20:50:02 -0800

    - Added `Disable Credit Loss At Company` and `Scale Credit Loss By Lobby Size` config options

--- CreditLossOnDeathConfigurator/CreditLossOnDeathConfigurator.cs
+++ CreditLossOnDeathConfigurator/CreditLossOnDeathConfigurator.cs
@@ -17,16 +17,20 @@ namespace CreditLossOnDeathConfigurator
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "GiGaGon";
         public const string PluginName = "CreditLossOnDeathConfigurator";
-        public const string PluginVersion = "1.0.1";
+        public const string PluginVersion = "1.0.2";
         private static ILHook _hook = null!;
         public static ConfigFile configFile = null!;
         public static ConfigEntry<float> creditPercentageLostPerUnrecoveredBody = null!;
-        public static ConfigEntry<float> creditPercentageLostPerRecoveredBody = null!;
+        public static ConfigEntry<float> creditPercentageLostPerRecoveredBody   = null!;
+        public static ConfigEntry<bool>  disableCreditLossAtCompany             = null!;
+        public static ConfigEntry<bool>  scaleCreditLossByLobbySize             = null!;
         private void Awake()
         {
             configFile = new ConfigFile(Path.Combine(Paths.ConfigPath, $"{PluginName}.cfg"), true);
             creditPercentageLostPerUnrecoveredBody = configFile.Bind("General", "Credit Percentage Lost Per Unrecovered Body", 0.20f, "");
             creditPercentageLostPerRecoveredBody   = configFile.Bind("General", "Credit Percentage Lost Per Recovered Body",   0.08f, "By default 8% is lost on recovered bodies, though in vanilla it is not stated nor displayed on the end card.");
+            disableCreditLossAtCompany             = configFile.Bind("General", "Disable Credit Loss At Company",              false, "Stops all credit loss from recovered/unrecovered bodies while at the company.");
+            scaleCreditLossByLobbySize             = configFile.Bind("General", "Scale Credit Loss By Lobby Size",             false, "Scales based on a 4 player lobby to make it so that if only 1 person survives, the credit loss is the same accross all sizes.\nExample: 4 player lobby with 20% unrecovered, max is 20% * 3 = 60%. \nA five player lobby would be 20% * 3 (base max) / 4 (max unrecoverable) = 16% per body, since 15% * 4 = 60%. \nA 3 player lobby would be 20% * 3 / 2 = 30% per.\nThe same math applies to the recovered body percentage.");
 
             _hook = new ILHook(
                 typeof(HUDManager).GetMethod("ApplyPenalty", BindingFlags.Instance | BindingFlags.Public), 
@@ -41,6 +45,8 @@ namespace CreditLossOnDeathConfigurator
         public void SetBodyCreditReductionMultipliers(ILContext il)
         {
             ILCursor c = new ILCursor(il);
+            // orig code: float num = 0.2f;
+            // replaces:              ^^^^
             c.GotoNext(
                 x => x.MatchLdcR4(0.2f)
             );
@@ -48,9 +54,22 @@ namespace CreditLossOnDeathConfigurator
             c.EmitDelegate<Func<float>>(() =>
             {
                 configFile.Reload();
-                return creditPercentageLostPerUnrecoveredBody.Value;
+                if (disableCreditLossAtCompany.Value && StartOfRound.Instance.currentLevel.sceneName == "CompanyBuilding")
+                {
+                    return 0f;
+                }
+                else if (scaleCreditLossByLobbySize.Value)
+                {
+                    return creditPercentageLostPerUnrecoveredBody.Value * (3 / (StartOfRound.Instance.connectedPlayersAmount - 1));
+                }
+                else
+                {
+                    return creditPercentageLostPerUnrecoveredBody.Value;
+                }
             });
 
+            // orig code: terminal.groupCredits -= (int)((float)groupCredits * (num / 2.5f));
+            // replaces:                                                              ^^^^
             c.Goto(0);
             c.GotoNext(
                 x => x.MatchLdloc(2),
@@ -61,9 +80,22 @@ namespace CreditLossOnDeathConfigurator
             c.Remove();
             c.EmitDelegate<Func<float>>(() =>
             {
-                return creditPercentageLostPerUnrecoveredBody.Value / creditPercentageLostPerRecoveredBody.Value;
+                if (disableCreditLossAtCompany.Value && StartOfRound.Instance.currentLevel.sceneName == "CompanyBuilding")
+                {
+                    return 0f;
+                }
+                else if (scaleCreditLossByLobbySize.Value)
+                {
+                    return creditPercentageLostPerUnrecoveredBody.Value / (creditPercentageLostPerRecoveredBody.Value * (3 / (StartOfRound.Instance.connectedPlayersAmount - 1)));
+                } 
+                else
+                {
+                    return creditPercentageLostPerUnrecoveredBody.Value / creditPercentageLostPerRecoveredBody.Value;
+                }
             });
 
+            // orig code: statsUIElements.penaltyAddition.text = $"{playersDead} casualties: -{num * 100f * (float)(playersDead - bodiesInsured)}%\n({bodiesInsured} bodies recovered)";
+            // replaces:                                                                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
             c.GotoNext(
                 x => x.MatchLdarg(1),
                 x => x.MatchBox<int>(),
@@ -75,7 +107,18 @@ namespace CreditLossOnDeathConfigurator
             c.Emit(OpCodes.Ldarg_2);
             c.EmitDelegate<Func<int, int, float>>((playersDead, bodiesInsured) =>
             {
-                return 100f * ((creditPercentageLostPerUnrecoveredBody.Value * (playersDead - bodiesInsured)) + (creditPercentageLostPerRecoveredBody.Value * bodiesInsured));
+                if (disableCreditLossAtCompany.Value && StartOfRound.Instance.currentLevel.sceneName == "CompanyBuilding")
+                {
+                    return 0f;
+                }
+                else if (scaleCreditLossByLobbySize.Value)
+                {
+                    return 100f * ((creditPercentageLostPerUnrecoveredBody.Value * (playersDead - bodiesInsured) * (3 / (StartOfRound.Instance.connectedPlayersAmount - 1))) + (creditPercentageLostPerRecoveredBody.Value * bodiesInsured * (3 / (StartOfRound.Instance.connectedPlayersAmount - 1))));
+                }
+                else
+                {
+                    return 100f * ((creditPercentageLostPerUnrecoveredBody.Value * (playersDead - bodiesInsured)) + (creditPercentageLostPerRecoveredBody.Value * bodiesInsured));
+                }
             });
         }
     }
